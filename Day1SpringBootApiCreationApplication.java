9443f00524eeae3947b7b9802ed61f661e7cbfce
Shruti Dhere <dhereshruti351@gmail.com>
2026-05-18 19:58:19 +0530

    Add files via upload

---
+++ Day1SpringBootApiCreationApplication.java
@@ -0,0 +1,15 @@
+package com.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1SpringBootApiCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day1SpringBootApiCreationApplication.class, args);
+	
+System.err.println(" Application Started ");
+	}
+
+}
